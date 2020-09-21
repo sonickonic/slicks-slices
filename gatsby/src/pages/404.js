151e3fce55bc9bff34f832456345ec17247f5ab6
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-21 21:03:28 +0100

    Add site pages
    - Gatsby core automatically turns React components
    in src/pages/* into pages.

---
+++ gatsby/src/pages/404.js
@@ -0,0 +1,5 @@
+import React from 'react';
+
+const FourOhFourPage = () => <p>404</p>;
+
+export default FourOhFourPage;
---
+++ gatsby/src/pages/slicemasters.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+const SliceMastersPage = () => (
+  <>
+    <p>slice masters page</p>
+  </>
+);
+
+export default SliceMastersPage;
