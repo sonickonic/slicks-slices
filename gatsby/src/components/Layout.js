495a902a114ad8b7cea45446b9f52bacce97cd8c
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-22 11:11:45 +0100

    Add global styles
    - createGlobalStyle
    
    A helper function to generate a special StyledComponent that handles
    global styles. Normally, styled components are automatically scoped
    to a local CSS class and therefore isolated from other components.
    In the case of createGlobalStyle, this limitation is removed and
    things like CSS resets or base stylesheets can be applied.

--- gatsby/src/components/Layout.js
+++ gatsby/src/components/Layout.js
@@ -1,9 +1,14 @@
 import React from 'react';
 import Nav from './Nav';
 import Footer from './Footer';
+import 'normalize.css';
+import GlobalStyles from '../styles/GlobalStyles';
+import Typography from '../styles/Typography';
 
 const Layout = ({ children }) => (
   <div>
+    <GlobalStyles />
+    <Typography />
     <Nav />
     {children}
     <Footer />
