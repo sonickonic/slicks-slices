03413d0f10cf26841a20113bdaaae45a15aff4fe
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-25 14:56:57 +0100

    Query data using GraphQL tag
    - graphql tag is a page query, which is a query for a specific page in
    a site. It is unique in that it can take query variables unlike Gatsby’s
    static queries.

--- gatsby/src/pages/pizzas.js
+++ gatsby/src/pages/pizzas.js
@@ -7,3 +7,28 @@ const PizzasPage = () => (
 );
 
 export default PizzasPage;
+
+export const query = graphql`
+  query PizzaQueary {
+    pizzas: allSanityPizza {
+      nodes {
+        name
+        id
+        slug {
+          current
+        }
+        toppings {
+          id
+          name
+        }
+        image {
+          asset {
+            fluid(maxWidth: 400) {
+              ...GatsbySanityImageFluid
+            }
+          }
+        }
+      }
+    }
+  }
+`;
