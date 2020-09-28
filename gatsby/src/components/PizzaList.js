2c863def4a8fc62a5db9ec9ad57925c7a59a1e7e
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-28 12:25:40 +0100

    Introduce grid to pizza-menu layout
    - Subgrid, Feature of the CSS Grid Layout Module Level 2 that allows a
    grid-item with its own grid to align in one or both dimensions with its
    parent grid.
    
    - This feature is shipped in Firefox 71, which is currently the *only*
    browser to implement subgrid.

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,24 +1,49 @@
 import React from 'react';
+import styled from 'styled-components';
+
 import { Link } from 'gatsby';
 import Img from 'gatsby-image';
 
+const PizzaGrid = styled.div`
+  display: grid;
+  grid-template-columns: repeat(auto-fill, minmax(300px, 1fr));
+  grid-gap: 4rem;
+  grid-auto-rows: auto auto 500px;
+`;
+
+const PizzaStyles = styled.div`
+  display: grid;
+  @supports not (grid-template-rows: subgrid) {
+    --row: auto auto 1fr;
+  }
+  grid-template-rows: var(--row, subgrid);
+  grid-row: span 3;
+  grid-gap: 1rem;
+  h2,
+  p {
+    margin: 0;
+  }
+`;
+
 function SinglePizza({ pizza }) {
   return (
-    <Link to={`/pizza/${pizza.slug.current}`}>
-      <h2 className="mark">{pizza.name}</h2>
+    <PizzaStyles>
+      <Link to={`/pizza/${pizza.slug.current}`}>
+        <h2 className="mark">{pizza.name}</h2>
+      </Link>
       <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
       <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
-    </Link>
+    </PizzaStyles>
   );
 }
 
 const PizzaList = ({ pizzas }) => (
   <>
-    <div>
+    <PizzaGrid>
       {pizzas.map((pizza) => (
         <SinglePizza key={pizza.id} pizza={pizza} />
       ))}
-    </div>
+    </PizzaGrid>
   </>
 );
 
