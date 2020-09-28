691151c6a674b7bf5218f4b7247a22c044c17612
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-09-28 11:21:04 +0100

    Add image component using gatsby-image
    - gatsby-image is a React component specially designed to work
    seamlessly with Gatsby’s GraphQL queries. It combines Gatsby’s native
    image processing capabilities with advanced image loading techniques to
    easily and completely optimize image loading for your sites.

--- gatsby/src/components/PizzaList.js
+++ gatsby/src/components/PizzaList.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import { Link } from 'gatsby';
+import Img from 'gatsby-image';
 
 function SinglePizza({ pizza }) {
   return (
     <Link to={`/pizza/${pizza.slug.current}`}>
       <h2 className="mark">{pizza.name}</h2>
       <p>{pizza.toppings.map((topping) => topping.name).join(', ')}</p>
+      <Img fluid={pizza.image.asset.fluid} alt={pizza.name} />
     </Link>
   );
 }
