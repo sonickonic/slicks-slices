9abe2b8844d386016acf17cc2c113ffb87eb4b78
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-10-05 15:14:48 +0100

    Style beers page

--- gatsby/src/pages/beers.js
+++ gatsby/src/pages/beers.js
@@ -1,9 +1,68 @@
+import { graphql } from 'gatsby';
 import React from 'react';
+import styled from 'styled-components';
 
-const BeersPage = () => (
+const BeerGrid = styled.div`
+  display: grid;
+  gap: 2rem;
+  grid-template-columns: repeat(auto-fit, minmax(200px, 1fr));
+`;
+
+const SingleBeer = styled.div`
+  border: 1px solid var(--grey);
+  padding: 2rem;
+  text-align: center;
+  img {
+    width: 100%;
+    height: 200px;
+    object-fit: contain;
+    display: block;
+    font-size: 10px;
+  }
+`;
+
+const BeersPage = ({ data }) => (
   <>
-    <p>beers page</p>
+    <h2 className="center">
+      We have {data.beers.nodes.length} Beers Available. Dine in Only!
+    </h2>
+    <BeerGrid>
+      {data.beers.nodes.map((beer) => {
+        const rating = Math.round(beer.rating.average);
+        return (
+          <SingleBeer key={beer.id}>
+            <img src={beer.image} alt={beer.name} />
+            <h3>{beer.name}</h3>
+            {beer.price}
+            <p title={`${rating} out of 5  stars`}>
+              {`⭐`.repeat(rating)}
+              <span style={{ filter: `grayscale(100%)` }}>
+                {`⭐`.repeat(5 - rating)}
+              </span>
+              <span>({beer.rating.reviews})</span>
+            </p>
+          </SingleBeer>
+        );
+      })}
+    </BeerGrid>
   </>
 );
 
 export default BeersPage;
+
+export const query = graphql`
+  query {
+    beers: allBeer {
+      nodes {
+        id
+        name
+        price
+        image
+        rating {
+          average
+          reviews
+        }
+      }
+    }
+  }
+`;
