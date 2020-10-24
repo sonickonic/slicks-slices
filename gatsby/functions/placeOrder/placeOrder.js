2065b5bd392ba2b4dd146db7fcc7f44ccbd20597
Sofia Fateyev <pateyev.sofia@gmail.com>
2020-10-24 17:58:41 +0100

    Update serverless function

--- gatsby/functions/placeOrder/placeOrder.js
+++ gatsby/functions/placeOrder/placeOrder.js
@@ -1,5 +1,26 @@
 const nodemailer = require('nodemailer');
 
+const generateOrderEmail = ({ order, total }) => `<div>
+    <h2>Your Recent Order for ${total}</h2>
+    <p>Please start walking over, we will have your order ready in the next 20 mins.</p>
+    <ul>
+      ${order
+        .map(
+          (item) => `<li>
+        <img src="${item.thumbnail}" alt="${item.name}"/>
+        ${item.size} ${item.name} - ${item.price}
+      </li>`
+        )
+        .join('')}
+    </ul>
+    <p>Your total is <strong>$${total}</strong> due at pickup</p>
+    <style>
+        ul {
+          list-style: none;
+        }
+    </style>
+  </div>`;
+
 const transporter = nodemailer.createTransport({
   host: process.env.MAIL_HOST,
   port: 587,
@@ -10,14 +31,30 @@ const transporter = nodemailer.createTransport({
 });
 
 exports.handler = async (event, context) => {
+  const body = JSON.parse(event.body);
+  const requireFields = ['email', 'name', 'order'];
+
+  for (const field of requireFields) {
+    console.log(`checking that ${field} is good`);
+    if (!body[field]) {
+      return {
+        statusCode: 400,
+        body: JSON.stringify({
+          message: `Oops! You are missing the ${field} field`,
+        }),
+      };
+    }
+  }
+
   const info = await transporter.sendMail({
     from: "Slick's Slices <slick@example.com>",
-    to: 'orders@example.com',
+    to: `${body.name} <${body.email}>, orders@example.com`,
     subject: 'New order!',
-    html: `<p>Your new pizza order is here!</p>`,
+    html: generateOrderEmail({ order: body.order, total: body.total }),
   });
+
   return {
     statusCode: 200,
-    body: JSON.stringify(info),
+    body: JSON.stringify({ message: 'Success' }),
   };
 };
